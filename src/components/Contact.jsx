b5d0afc49f972135e87ae389eff050e3072f0900
ChidiogoVictor <chidiogovictor10@gmail.com>
2025-02-14 14:41:39 +0100

    create contact us form

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -1,13 +1,38 @@
-import React from "react";
+import { useRef } from "react";
 import Section from "./Section";
+import Button from "./Button";
 
 const Contact = () => {
+  const form = useRef();
+
+  const sendEmail = () => {};
   return (
-    <Section>
-      <div className="container" id="contact">
-        <div className="relative">
-          <h2 ClassName=" h2 text-center">Contact Us</h2>
-        </div>
+    <Section className="container" id="contact">
+      <div className="relative">
+        <h2 className="h2 text-center mb-4">Contact Us</h2>
+        <form
+          ref={form}
+          onSubmit={sendEmail}
+          className="--form-control --card flex flex-col items-center justify-center"
+        >
+          <input
+            type="text"
+            placeholder="Full Name"
+            name="user_name"
+            required
+          />
+          <input type="email" placeholder="Email" name="user_email" required />
+          <input type="text" placeholder="Subject" name="subject" required />
+          <textarea
+            name="message"
+            placeholder="Message"
+            cols="22"
+            rows="10"
+            required
+            className="p-2"
+          />
+          <Button type="submit">{"Send Message"}</Button>
+        </form>
       </div>
     </Section>
   );
