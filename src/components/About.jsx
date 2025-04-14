152fd37967c6958d251cd2ddb001e402f636164f
ChidiogoVictor <chidiogovictor10@gmail.com>
2025-04-14 14:58:54 +0100

    fixed bug in about page

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -16,29 +16,29 @@ const About = () => {
             tailored to meet your business goals.
             <p className="h4 text-center text-color-1 py-5">Why Choose Us?</p>
             <div className="flex items-center justify-center px-4">
-              <ul className="w-full">
-                <li className="flex items-justify py-5 border-n-6 rounded-[2rem] items-center justify-center">
+              <ul className="w-full ">
+                <li className="flex items-justify py-5 border-n-6 rounded-[2rem] items-center justify-start">
                   <img src={check} width={24} height={24} alt="Check" />
                   <p className="body-2 ml-4">
                     Inovation-Driven Approach - We stay ahead of trends to build
-                    future-proof solutions
+                    future-proof solutions.
                   </p>
                 </li>
-                <li className="flex items-justify py-5 border-n-6 rounded-[2rem] items-center justify-center">
+                <li className="flex items-justify py-5 border-n-6 rounded-[2rem] items-center justify-start">
                   <img src={check} width={24} height={24} alt="Check" />
                   <p className="body-2 ml-4">
                     Client-Centric Solutions - Your vision is our priority, we
                     customize our services to meet your unique needs.
                   </p>
                 </li>
-                <li className="flex items-justify py-5 border-n-6 rounded-[2rem] items-center justify-center">
+                <li className="flex items-justify py-5 border-n-6 rounded-[2rem] items-center justify-start">
                   <img src={check} width={24} height={24} alt="Check" />
                   <p className="body-2 ml-4">
                     Technical Excellence - We leverage the latest technologies
                     for efficient and scalable web development.
                   </p>
                 </li>
-                <li className="flex items-justify py-5 border-n-6 rounded-[2rem] items-center justify-center">
+                <li className="flex items-justify py-5 border-n-6 rounded-[2rem] items-center justify-start">
                   <img src={check} width={24} height={24} alt="Check" />
                   <p className="body-2 ml-4">
                     Reliable Support - We are always here to assist, ensuring
